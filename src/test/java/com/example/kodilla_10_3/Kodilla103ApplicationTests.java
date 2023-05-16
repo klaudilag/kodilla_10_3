9551e3453b6fc6d07a9abba1e5d972ce6597a77f
klaudilag <klaudilag@wp.pl>
2023-05-16 21:03:55 +0200

    Dodanie asercji w testach

--- src/test/java/com/example/kodilla_10_3/Kodilla103ApplicationTests.java
+++ src/test/java/com/example/kodilla_10_3/Kodilla103ApplicationTests.java
@@ -1,20 +1,37 @@
 package com.example.kodilla_10_3;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.ArrayList;
+
 @SpringBootTest
 class BoardTestSuite{
+    ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+    Board board = context.getBean(Board.class);
     @Test
-    void testTaskAdd(){
-        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
-        Board board = context.getBean(Board.class);
+    void testVoid(){
         board.getDoneList().addTask("Zadanie1");
         board.getInProgressList().addTask("Zadanie2");
         board.getAll();
         board.getDoneList().removeTask("Zadanie1");
         board.getAll();
+    }
+    @Test
+    void testTaskAdd(){
+        board.getDoneList().addTask("Zadanie1");
+        ArrayList<String> arrayForTest = new ArrayList<>();
+        arrayForTest.add("Zadanie1");
+        Assertions.assertEquals(board.getDoneList().getTasks(), arrayForTest);
+    }
+    @Test
+    void testTaskRemove(){
+        board.getDoneList().addTask("Zadanie1");
+        board.getDoneList().removeTask("Zadanie1");
+        ArrayList<Board> emptyList = new ArrayList<>();
+        Assertions.assertEquals(board.getDoneList().getTasks(),emptyList);
     }
 }
